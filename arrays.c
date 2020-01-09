9f4913f3dffcd01359d71349eee269a90287c6c0
Kenneth Zhang <zhangk3714@my.sduhsd.net>
2020-01-09 12:29:10 -0800

    add arrays.c

--- arrays.c
+++ arrays.c
@@ -14,29 +14,6 @@ void arrayAdd(int arr[], int s, int n)
 
 int main()
 {
-  // create an int array 5 long
-  /*
-   int arr[5];
-
-  for (int i = 0; i < 5; i++)
-  {
-    arr[i] = i * i;
-  }
-
-  printf("1st and 3rd vals: %d, %d\n", arr[0], arr[2]);
-
-  printf("Or %d, %d\n", *arr, *(arr+2));
-
-
-  // create array to hold intialized values
-  int arr2[] = { 1, 2, 3, 4 };
-  for (int i = 0; i < 4; i++)
-  {
-    printf("%d, ", arr2[i]);
-  }
-  printf("\n");
-
-  */
 
   // the actual thing
   int arr3[100];
